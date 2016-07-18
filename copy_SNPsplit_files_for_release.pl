93d5b81dcd2db2d51888289f968b8a144aa467bd
FelixKrueger <felix.krueger@babraham.ac.uk>
2016-07-18 11:02:04 +0100

    Added more information about SNP filtering and processing to the manual

---
+++ copy_SNPsplit_files_for_release.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use File::Copy "cp";
+
+my $dir = shift@ARGV;
+
+die "Please provide a directory to copy files to!\n\n" unless ($dir);
+
+unless (-d $dir){
+  warn "Specified directory '$dir' doesn't exist. Creating it for you...\n\n";
+  mkdir $dir or die "Failed to create directory: $!\n\n";
+}
+
+my @files = ('RELEASE_NOTES.txt','SNPsplit','tag2sort','SNPsplit_User_Guide.pdf','license.txt','SNPsplit_genome_preparation');
+
+foreach my $file (@files){
+  copy_and_warn($file);
+}
+
+sub copy_and_warn{
+    my $file = shift;
+    warn "Now copying '$file' to $dir\n";
+    cp($file,"$dir/") or die "Copy failed: $!";
+    
+}
+
+@files = ('SNPsplit','tag2sort','SNPsplit_genome_preparation');
+
+foreach my $file (@files){
+    set_permissions($file);
+}
+
+sub set_permissions{
+    my $file = shift;
+    warn "Setting permissions for ${dir}/$file\n";
+    chmod 0755, "${dir}/$file";
+}
+
+
+### Taring up the folder
+$dir =~ s/\/$//;
+warn "Tar command:\ntar czvf ${dir}.tar.gz $dir\n\n";
+system ("tar czvf ${dir}.tar.gz $dir/");
